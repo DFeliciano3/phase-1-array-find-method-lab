71c98de9c07b5def82cc94b8e5ba3652f57ea802
DFeliciano3 <91075867+DFeliciano3@users.noreply.github.com>
2022-05-27 12:10:06 -0400

    commit tree fad3166125a3b6758db0584d55db1f9838c93361

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    let recordResult = record.find(recordYear => recordYear.result === "W")
+    if (recordResult === undefined){
+
+    }
+    else{
+        return recordResult.year
+        }
+    }
